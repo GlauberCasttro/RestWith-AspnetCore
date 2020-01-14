8a005a2b5c170d24f626eca0031a569efce43a27
Glauber Castro <glaubercasttro@gmail.com>
2020-01-14 12:09:55 -0300

    Subind version

---
+++ RestWebApiAspnetCore/RestWebApiAspnetCore/util/Utilitario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWebApiAspnetCore.util
+{
+    public static class Utilitario
+    {
+        public static decimal ConvertToDecimal(string number)
+        {
+            return Decimal.TryParse(number, out decimal decimalValue) ? decimalValue : 0;
+        }
+
+        public static bool IsNumeric(string strNumber)
+        {
+            double number;
+
+            bool isNumber = Double.TryParse(strNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
